3dcba717fe111d11ef6dd5cf21493f57ee6eb41d
frehiywot nega <fr.nega@gmail.com>
2022-11-17 15:19:18 +0300

    added api-config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import './style.css';
-import { viewScores } from './score.js';
+import { addScore, displayScorevalue } from './score';
 
-viewScores();
+const form = document.querySelector('form');
+const refresh = document.getElementById('refresh');
+displayScorevalue();
+
+refresh.addEventListener('click', () => {
+  displayScorevalue();
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const user = document.getElementById('name-input').value;
+  const score = document.getElementById('score-input').value;
+  if (user !== '' && score !== '') {
+    const data = {
+      user,
+      score,
+    };
+    addScore(data);
+    form.reset();
+  }
+});
